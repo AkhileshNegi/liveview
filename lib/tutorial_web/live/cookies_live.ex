ae21b102d59e5f1993fa980d83f634afb748fd45
Akhilesh Negi <akhileshnegi.an3@gmail.com>
2025-02-16 10:00:20 +0530

    clickable counter on cookie

--- lib/tutorial_web/live/cookies_live.ex
+++ lib/tutorial_web/live/cookies_live.ex
@@ -3,10 +3,12 @@ defmodule TutorialWeb.CookiesLive do
 
   def render(assigns) do
     ~H"""
-    <div class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-purple-900 to-red-900 text-white">
+    <div class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-purple-100 to-red-900 text-black">
       <h1 class="absolute top-5 left-5 text-2xl font-bold">Cookie Clicker</h1>
+      <div class="text-6xl mb-4">{@count}</div>
       <div
         class="text-9xl cursor-pointer select-none transition-transform duration-100 hover:scale-110 active:scale-90"
+        phx-click="increment"
       >
         🍪
       </div>
@@ -15,6 +17,11 @@ defmodule TutorialWeb.CookiesLive do
   end
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, count: 0)}
+  end
+
+  def handle_event("increment", _params, socket) do
+    new_count = socket.assigns.count + 1
+    {:noreply, assign(socket, :count, new_count)}
   end
 end
