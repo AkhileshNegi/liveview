3c35f3e20216900a5cd4580989505fadde70e3f0
Akhilesh Negi <akhileshnegi.an3@gmail.com>
2025-02-20 21:03:51 +0530

    using genserver cast

---
+++ lib/tutorial/poll_results.ex
@@ -0,0 +1,43 @@
+defmodule Tutorial.PollResults do
+  use GenServer
+
+  @initial_state %{"A" => 0, "B" => 0, "C" => 0, "D" => 0}
+
+  # Client API
+
+  def start do
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def get_state do
+    GenServer.call(__MODULE__, :get_state)
+  end
+
+  def vote(option) when option in ["A", "B", "C", "D"] do
+    GenServer.cast(__MODULE__, {:vote, option})
+  end
+
+  def reset do
+    GenServer.cast(__MODULE__, :reset)
+  end
+
+  # Server callbacks
+
+  def init(_) do
+    {:ok, @initial_state}
+  end
+
+  def handle_call(:get_state, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:vote, option}, state) do
+    new_state = Map.update!(state, option, &(&1 + 1))
+
+    {:noreply, new_state}
+  end
+
+  def handle_cast(:reset, _state) do
+    {:noreply, @initial_state}
+  end
+end
